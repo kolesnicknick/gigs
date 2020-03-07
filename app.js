fc9c75c355e2dc10495c289ff5ee1ccd7f82bd25
niko <kolesniknikolai92@gmail.com>
2020-03-07 16:44:59 +0200

    fix bug with css returns 404

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -40,6 +39,8 @@ app.use('/gigs', gigsRouter);
 //HandleBars
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   next(createError(404));
